84ee9ce37f8763389042a250e142ae37e74f4600
thuyduyen <leduyenduyen3004123@gmail.com>
2024-11-07 17:30:17 +0700

    fix pushed folder

--- src/main/java/pageObjects/Railway/MyTicketPage.java
+++
@@ -1,7 +0,0 @@
-package pageObjects.Railway;
-
-import org.openqa.selenium.By;
-
-public class MyTicketPage extends GeneralPage {
-    private By buttonCancel = By.xpath("/html/body/div/div[2]/form/div/div/table/tbody/tr[2]/td[11]/input");
-}
--- src/main/java/pageObjects/Railway/ResetPasswordTest.java
+++
@@ -1,4 +0,0 @@
-package pageObjects.Railway;
-
-public class ResetPasswordTest {
-}
